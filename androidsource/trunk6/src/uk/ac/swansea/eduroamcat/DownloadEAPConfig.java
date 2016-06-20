2455978d6346571f8bfb604f99eee351fe8160c1
swansea.ayres <swansea.ayres@2d91f571-0823-40d5-aea7-c74eced9d4e5>
2016-06-20 13:31:45 +0000

    tidied up downloadEAPConfig class

--- androidsource/trunk6/src/uk/ac/swansea/eduroamcat/DownloadEAPConfig.java
+++ androidsource/trunk6/src/uk/ac/swansea/eduroamcat/DownloadEAPConfig.java
@@ -16,24 +16,11 @@ class DownloadEAPConfig extends AsyncTask<String, Integer, Integer> {
     @Override
     protected Integer doInBackground(String... aurl) {
         int code=0;
-        try { 	    
+        try {
             URL url = new URL(aurl[0]);
             URLConnection connection = url.openConnection();
             eduroamCAT.debug("starting download...");
             eduroamCAT.downloaded=false;
-            connection.setConnectTimeout(5000);
-            connection.setReadTimeout(5000);
-            connection.setUseCaches(false);
-            connection.connect();
-            if ( connection instanceof HttpURLConnection) {
-                HttpURLConnection httpConnection = (HttpURLConnection) connection;
-
-                code = httpConnection.getResponseCode();
-                eduroamCAT.debug("response code="+code);
-            }
-//            int fileLength = connection.getContentLength();
-//            int tickSize = 2 * fileLength / 100;
-//            int nextProgress = tickSize;
             InputStream input = new BufferedInputStream(url.openStream());
             String path = Environment.getExternalStorageDirectory().getPath() + "/EAPConfig/";
             File file = new File(path);
@@ -41,7 +28,6 @@ class DownloadEAPConfig extends AsyncTask<String, Integer, Integer> {
             File outputFile = new File(file, "eduroam.eap-config");
             OutputStream output = new FileOutputStream(outputFile);
             byte data[] = new byte[1024 * 1024];
-            long total = 0;
             int count;
             while ((count = input.read(data)) != -1) {
                 output.write(data, 0, count);
@@ -50,11 +36,13 @@ class DownloadEAPConfig extends AsyncTask<String, Integer, Integer> {
             output.close();
             input.close();
             eduroamCAT.debug("ending download");
+            code=1;
 
         } catch (Exception e) {
         	eduroamCAT.debug("Error writing file to external storage");
+            code=0;
         }
-        eduroamCAT.downloaded=true;
+        if (code>0) eduroamCAT.downloaded=true;
         return code;
     }
 
