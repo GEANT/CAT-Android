9d71074b7977ec8aff5ef7523ceb54cc01db79cc
Gareth Ayres <gareth.ayres@gmail.com>
2017-07-07 10:12:04 +0100

    EAP-PWD UI - remove anon id feeback Github enhancement issue 7

--- src/uk/ac/swansea/eduroamcat/ConnectFragment.java
+++ src/uk/ac/swansea/eduroamcat/ConnectFragment.java
@@ -53,7 +53,7 @@ public class ConnectFragment extends Fragment implements OnClickListener
 	Activity activity = getActivity();
 	static TextView state1,state2,state3,state4,state5,state6,warning,passText;
 	static ImageView state1_image,state2_image,state3_image,state4_image,state5_image,state6_image;
-	static LinearLayout feedback,state5_layout;
+	static LinearLayout feedback,state5_layout,state2_layout;
 	private static WifiManager wifi = null;
 	static Boolean installedOK = false;
 	
@@ -163,9 +163,14 @@ public class ConnectFragment extends Fragment implements OnClickListener
 					state5.setText(idp_summary);
 				}
 				else {
+					//ca cert feedback
 					state5_image.setVisibility(View.GONE);
 					state5.setVisibility(View.GONE);
 					state5_layout.setVisibility(View.GONE);
+					//annon id feedback
+					state2_image.setVisibility(View.GONE);
+					state2.setVisibility(View.GONE);
+					state2_layout.setVisibility(View.GONE);
 				}
 			idp_summary = Html.fromHtml(state6_text);
 			state6.setText(idp_summary);
@@ -356,6 +361,7 @@ public class ConnectFragment extends Fragment implements OnClickListener
         state6_image = (ImageView) v.findViewById(R.id.imageView6);
         feedback = (LinearLayout) v.findViewById(R.id.feedback);
 		state5_layout = (LinearLayout) v.findViewById(R.id.state5_layout);
+		state2_layout = (LinearLayout) v.findViewById(R.id.state2_layout);
         //ssids.setOnItemClickListener(this);
         warning = (TextView) v.findViewById(R.id.textView3);
         username = (EditText) v.findViewById(R.id.username);
